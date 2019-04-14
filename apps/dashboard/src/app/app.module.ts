3014e04d71f820af2d22c6507e5853850dc8df9f
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-15 00:46:20 +0100

    angular-core: added app-routing module and created app routing

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { NxModule } from '@nrwl/nx';
 import { MaterialModule } from '@workshop/material';
-import { HomeModule } from './home/home.module';
-import { CustomersModule } from './customers/customers.module';
-import { ProjectsModule } from './projects/projects.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UiLoginModule } from '@workshop/ui-login';
 
 
 @NgModule({
@@ -16,12 +13,10 @@ import { ProjectsModule } from './projects/projects.module';
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
-    CustomersModule,
-    HomeModule,
     MaterialModule,
-    ProjectsModule
+    UiLoginModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
