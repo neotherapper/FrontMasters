ca1b7fc729286221e2d62701979322be34108873
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-14 22:49:34 +0100

    angular-core: added material lib to project

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NxModule } from '@nrwl/nx';
+import { MaterialModule } from '@workshop/material';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
